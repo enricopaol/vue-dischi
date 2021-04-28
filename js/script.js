562789a85ac016e34244aa6aaa4e28c51c2cbf01
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-28 12:55:30 +0200

    Infrastruttura base

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'test'
+        },
+        methods: {
+
+        },
+        mounted() {
+            
+        }
+    }
+);
