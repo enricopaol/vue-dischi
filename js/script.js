366a8559c2939c1c9d4eabd1e2375d59f437d5b3
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-28 15:13:50 +0200

    Printed albums

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            message: 'test'
+            albums: []
         },
         methods: {
 
         },
         mounted() {
-            
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((response) => {
+                    let result = response.data;
+                    this.albums = result.response;
+                    console.log(this.albums)
+                })
         }
     }
 );
